daee421a768a26d942bd5a3e166a8ddc4cd087ae
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-31 16:53:21 -0700

    create Button styled component for Pitch Tester

---
+++ client/src/PitchTester/Button.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #5ac494;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 20px;
+  border-radius: 8px;
+  border: none;
+`;
+
+export default Button;
